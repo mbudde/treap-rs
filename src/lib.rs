f4d389f2cbb0adf5c0efd3db401187babf8186d8
Michael Budde <mbudde@gmail.com>
2015-02-07 14:18:44 +0100

    Implement moving iterator

--- src/lib.rs
+++ src/lib.rs
@@ -9,10 +9,6 @@ pub struct Treap<K, V> {
     root: Option<Box<Node<K, V>>>,
 }
 
-pub struct Iter<'a, K: 'a, V: 'a> {
-    nodes: Vec<&'a Node<K, V>>,
-}
-
 impl<K: Ord, V> Treap<K, V> {
 
     /// Create an empty treap.
@@ -20,9 +16,11 @@ impl<K: Ord, V> Treap<K, V> {
     /// # Example
     ///
     /// ```
-    /// use treap::Treap;
-    /// let mut t = Treap::new();
+    /// let mut t = treap::Treap::new();
     /// t.insert(5, "yellow");
+    /// if let Some(s) = t.get(&5) {
+    ///     println!("{}", s);
+    /// }
     /// ```
     pub fn new() -> Treap<K, V> {
         Treap { root: None }
@@ -75,6 +73,8 @@ impl<K: Ord, V> Treap<K, V> {
 
     /// Return an iterator over keys and values in the treap. The order is arbitrary.
     ///
+    /// # Example
+    ///
     /// ```
     /// let mut t = treap::Treap::new();
     /// t.extend(vec![(1, "red"), (2, "blue"), (3, "green")].into_iter());
@@ -93,6 +93,49 @@ impl<K: Ord, V> Treap<K, V> {
         }
     }
 
+    /// Return an iterator that moves keys and values out of treap. The order is arbitrary.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// let mut t = treap::Treap::new();
+    /// t.extend(vec![(1, "red"), (2, "blue"), (3, "green")].into_iter());
+    ///
+    /// // Print keys and values in arbitrary order.
+    /// for (k, v) in t.into_iter() {
+    ///     println!("{}: {}", k, v);
+    /// }
+    /// ```
+    pub fn into_iter(self) -> IntoIter<K, V> {
+        IntoIter {
+            nodes: match self.root {
+                None => Vec::new(),
+                Some(n) => vec![*n]
+            }
+        }
+    }
+}
+
+impl<K: Ord, V> Extend<(K, V)> for Treap<K, V> {
+    #[inline]
+    fn extend<T: Iterator<Item=(K, V)>>(&mut self, mut iter: T) {
+        for (k, v) in iter {
+            self.insert(k, v);
+        }
+    }
+}
+
+impl<K: Ord, V> FromIterator<(K, V)> for Treap<K, V> {
+    #[inline]
+    fn from_iter<T: Iterator<Item=(K, V)>>(iter: T) -> Treap<K, V> {
+        let mut treap = Treap::new();
+        treap.extend(iter);
+        treap
+    }
+}
+
+pub struct Iter<'a, K: 'a, V: 'a> {
+    nodes: Vec<&'a Node<K, V>>,
 }
 
 impl<'a, K, V> Iterator for Iter<'a, K, V> {
@@ -114,20 +157,25 @@ impl<'a, K, V> Iterator for Iter<'a, K, V> {
     }
 }
 
-impl<K: Ord, V> Extend<(K, V)> for Treap<K, V> {
-    #[inline]
-    fn extend<T: Iterator<Item=(K, V)>>(&mut self, mut iter: T) {
-        for (k, v) in iter {
-            self.insert(k, v);
-        }
-    }
+pub struct IntoIter<K, V> {
+    nodes: Vec<Node<K, V>>,
 }
 
-impl<K: Ord, V> FromIterator<(K, V)> for Treap<K, V> {
-    #[inline]
-    fn from_iter<T: Iterator<Item=(K, V)>>(iter: T) -> Treap<K, V> {
-        let mut treap = Treap::new();
-        treap.extend(iter);
-        treap
+impl<K, V> Iterator for IntoIter<K, V> {
+    type Item = (K, V);
+
+    fn next(&mut self) -> Option<(K, V)> {
+        match self.nodes.pop() {
+            None => None,
+            Some(node) => {
+                if let Some(boxed) = node.left {
+                    self.nodes.push(*boxed);
+                }
+                if let Some(boxed) = node.right {
+                    self.nodes.push(*boxed);
+                }
+                Some((node.key, node.value))
+            }
+        }
     }
 }
